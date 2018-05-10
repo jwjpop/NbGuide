47a30a94ba99e941110994e67a59437ae62ec78c
cowooding <jwjppp@naver.com>
2018-05-10 21:23:41 +0900

    정보페이지에 지도버튼 메뉴판 1,2 버튼을 만들었고 액티비티에 띄우는 방법을 이용했다.

--- DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/InfoPage/Info.java
+++ DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/InfoPage/Info.java
@@ -1,13 +1,16 @@
 package com.example.woojinroom.daeran.TapPage.InfoPage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.woojinroom.daeran.R;
+import com.example.woojinroom.daeran.TapPage.MainPage.WritePage.WriteActivity;
 
 /**
  * Created by woojinroom on 2018-01-31.
@@ -21,13 +24,37 @@ public class Info extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view;
         view =  inflater.inflate(R.layout.fragment_info, container, false);
 
         map =(Button)view.findViewById(R.id.button_map);
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"준비중입니다.",Toast.LENGTH_SHORT).show();
+            }
+        });
+
         menu1 =(Button)view.findViewById(R.id.button_menu1);
+        menu1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menu1_intent = new Intent(getContext(), ImageActivity.class);
+                menu1_intent.putExtra("menu",1);
+                startActivity(menu1_intent);
+            }
+        });
+
         menu2 =(Button)view.findViewById(R.id.button_menu2);
+        menu2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent menu2_intent = new Intent(getContext(), ImageActivity.class);
+                menu2_intent.putExtra("menu",2);
+                startActivity(menu2_intent);
+            }
+        });
         return view;
     }
 }
