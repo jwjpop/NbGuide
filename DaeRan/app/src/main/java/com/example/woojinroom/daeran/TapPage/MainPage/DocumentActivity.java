e5ffd674c6d1f111f792efdab450851ac18badd3
woojin <jwjppp@naver.com>
2018-07-08 23:11:34 +0900

    글 읽는 부분에서 돌아가기 버튼 만듬

--- DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/MainPage/DocumentActivity.java
+++ DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/MainPage/DocumentActivity.java
@@ -1,10 +1,14 @@
 package com.example.woojinroom.daeran.TapPage.MainPage;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
+import com.example.woojinroom.daeran.MainActivity;
 import com.example.woojinroom.daeran.R;
 
 public class DocumentActivity extends AppCompatActivity {
@@ -12,7 +16,7 @@ public class DocumentActivity extends AppCompatActivity {
     Toolbar toolbar;
     TextView toolbar_user,title,color,number,price,content,date;
 
-
+    ImageButton imageButtonLeft;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_document);
@@ -21,6 +25,16 @@ public class DocumentActivity extends AppCompatActivity {
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar_user = (TextView) toolbar.findViewById(R.id.title);
         toolbar_user.setText("USER");
+
+        imageButtonLeft = (ImageButton) toolbar.findViewById(R.id.imagebutton_left);
+        imageButtonLeft.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent refresh_intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(refresh_intent);
+                finish();
+            }
+        });
 
         title = (TextView)findViewById(R.id.doc_title);
         color = (TextView)findViewById(R.id.doc_color);
