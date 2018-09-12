e0d7590caa8dc2cfb72b68047d28b9793823a628
woojin <jwjppp@naver.com>
2018-09-12 21:54:56 +0900

    이미지 페이지를 프레임레이아웃으로 수정하여 입장정보 페이지 구현

--- DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/InfoPage/ImageActivity.java
+++ DaeRan/app/src/main/java/com/example/woojinroom/daeran/TapPage/InfoPage/ImageActivity.java
@@ -5,10 +5,15 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.woojinroom.daeran.R;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by woojinroom on 2018-05-10.
@@ -18,6 +23,11 @@ public class ImageActivity extends AppCompatActivity {
     Button back;
     PhotoView mimageView;
     int menu;
+    String info;
+    TextView tv_info;
+
+    private FirebaseDatabase mDatabase;
+    private DatabaseReference mReference;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,24 +42,56 @@ public class ImageActivity extends AppCompatActivity {
         });
 
         mimageView =(PhotoView) findViewById(R.id.image_info);
-
-        Intent get_intent;
+        tv_info = (TextView) findViewById(R.id.tv_info);
+        final Intent get_intent;
         get_intent = getIntent();
         menu = get_intent.getIntExtra("menu",1);
 
-        if(menu==1){
+        if(menu==1)
+        {
+            tv_info.setVisibility(View.INVISIBLE);
+            mimageView.setVisibility(View.VISIBLE);
             mimageView.setImageResource(R.drawable.menu1_180503);
         }else if(menu==2)
         {
+            tv_info.setVisibility(View.INVISIBLE);
+            mimageView.setVisibility(View.VISIBLE);
             mimageView.setImageResource(R.drawable.menu2_180503);
         }
         else if(menu==3)
         {
+            tv_info.setVisibility(View.INVISIBLE);
+            mimageView.setVisibility(View.VISIBLE);
             mimageView.setImageResource(R.drawable.map_1f);
         }
         else  if (menu==4)
         {
+            tv_info.setVisibility(View.INVISIBLE);
+            mimageView.setVisibility(View.VISIBLE);
             mimageView.setImageResource(R.drawable.map_2f);
+        }
+        else  if (menu==5)
+        {
+            mimageView.setVisibility(View.INVISIBLE);
+            tv_info.setVisibility(View.VISIBLE);
+            mDatabase = FirebaseDatabase.getInstance();
+            mReference = mDatabase.getReference("enterinfo"); // 변경값을 확인할 child 이름
+
+            mReference.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot messageData : dataSnapshot.getChildren()) {
+                        info = messageData.getValue().toString();
+                    }
+
+                    tv_info.setText(info);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
         }
 
     }
