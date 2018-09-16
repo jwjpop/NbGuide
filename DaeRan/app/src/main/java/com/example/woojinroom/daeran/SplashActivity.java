4ffc6f4f8511ab1ab14eae8c161dc3d9ccb10ed7
cowooding <jwjppp@naver.com>
2018-09-17 01:26:09 +0900

    splash 와 icon 적용

--- DaeRan/app/src/main/java/com/example/woojinroom/daeran/SplashActivity.java
+++ DaeRan/app/src/main/java/com/example/woojinroom/daeran/SplashActivity.java
@@ -12,7 +12,7 @@ public class SplashActivity extends AppCompatActivity {
         setContentView(R.layout.activity_splash);
 
         try {
-            Thread.sleep(3000);
+            Thread.sleep(30000);
             Intent intent = new Intent(this, MainActivity.class);
             startActivity(intent);
             finish();
