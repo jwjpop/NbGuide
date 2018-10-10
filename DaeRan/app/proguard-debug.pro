6235b9c8c8043cd30a42b7d31b13304e0734d4d1
woojin <jwjppp@naver.com>
2018-10-10 14:13:18 +0900

    스토리지 연결 작업 중

--- DaeRan/app/proguard-debug.pro
+++ DaeRan/app/proguard-debug.pro
@@ -4,5 +4,27 @@
 -keepattributes SoureFile,LineNumberTable,Signature   #소스파일, 라인 전보 유지
 -keepattributes *Annotation*, InnerClasses, EnclosingMethod
 
+-keepclassmembers class com.cowooding.nbguide.DB.**{ *;}
+-keepclassmembers class com.cowooding.nbguide.TapPage.Chat.**{ *;}
+-keepclassmembers class com.cowooding.nbguide.TapPage.MainPage.**{ *;}
+-keepclassmembers class com.cowooding.nbguide.TapPage.MyPage.**{ *;}
 -keepclassmembers class com.cowooding.nbguide.TapPage.PlayListPage.**{ *;}
+
+# AdMob 사용을 위한 설정
+-keep public class com.google.android.gms.ads.**{
+    public *;
+}
+-keep public class com.google.ads.**{
+    public *;
+}
+
+# Cauly 사용을 위한 설정
+-keep public class com.fsn.cauly.** {
+	public protected *;
+}
+-keep public class com.trid.tridad.** {
+	public protected *;
+}
+-dontwarn android.webkit.**
+
 # End: Debug ProGuard rules
--- DaeRan/app/src/main/java/com/cowooding/nbguide/SplashActivity.java
+++ DaeRan/app/src/main/java/com/cowooding/nbguide/SplashActivity.java
@@ -3,13 +3,26 @@ package com.cowooding.nbguide;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView img_splash;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        img_splash = (ImageView)findViewById(R.id.img_splash);
+        FirebaseStorage fs = FirebaseStorage.getInstance();
+        StorageReference imagesRef = fs.getReference().child("image/drawable-xhdpi/image_splash.png");
+        Glide.with(this)
+                .load(imagesRef)
+                .into(img_splash);
 
         try {
             Thread.sleep(2000);
